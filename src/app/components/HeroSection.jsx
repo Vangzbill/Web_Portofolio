ab0bea1db9a63be7a04d0fcd09f8c7fa2e1cb6da
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2024-01-25 12:13:02 +0700

    revisi syntax

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -41,7 +41,7 @@ const HeroSection = () => {
           >
             <h1 className="text-white mb-4 text-3xl sm:text-4xl lg:text-7xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Hello, I'&apos;'m{" "}
             </span>
             <br></br>
             <TypeAnimation
